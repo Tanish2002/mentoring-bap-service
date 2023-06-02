0e1e993bf3df0086f821b6dca7d305971a9bb574
Tanish2002 <tanishkhare@gmail.com>
2023-06-02 15:32:52 +0530

    feat: add localtunnel support

--- src/utils/contextBuilder.js
+++ src/utils/contextBuilder.js
@@ -1,11 +1,22 @@
 'use strict'
 
+const localtunnel = require('localtunnel');
+const { ConnectionTimeoutError } = require('redis');
+async function getUrl() {
+	const tunnel = await localtunnel({ port: 3015 });
+	return `${tunnel.url}/osl-bap/dsep`;
+};
 exports.contextBuilder = async (transactionId, messageId, action, bppId, bppUri) => {
+	const tunnelURI = await getUrl().then(result => {
+		return result;
+	}).catch(err => {
+		console.log(err);
+	})
 	const context = {
 		domain: process.env.DOMAIN,
 		action,
 		bap_id: process.env.BAP_ID,
-		bap_uri: process.env.BAP_URI,
+		bap_uri: process.env.BAP_URI || tunnelURI,
 		bpp_id: bppId,
 		bpp_uri: bppUri,
 		timestamp: new Date(),
